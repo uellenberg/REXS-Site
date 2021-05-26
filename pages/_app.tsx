72794968c75d6a0ed58116d9709cec60f6e9c66e
uellenberg <JonahUellenberg@hotmail.com>
2021-05-25 20:22:10 -0700

    Setup next

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import {ChakraProvider, extendTheme} from "@chakra-ui/react"
+import React from "react";
+
+import "../styles/index.scss";
+import "prismjs/themes/prism-tomorrow.css";
+
+import "../js/index";
+
+const config = {
+    initialColorMode: "dark",
+    useSystemColorMode: false
+}
+// @ts-ignore
+const theme = extendTheme({ config });
+
+export const App = ({ Component, pageProps}) => {
+    return (
+        <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    );
+}
+
+export default App;
